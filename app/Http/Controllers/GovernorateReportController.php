7fd96d00c9ded64b34e61f0076447e47f1fd4845
Shady Alfred <shadyalfred98@gmail.com>
2020-02-20 00:04:01 +0200

    Fix MySQL SQLSTATE[42000]

--- app/Http/Controllers/GovernorateReportController.php
+++ app/Http/Controllers/GovernorateReportController.php
@@ -27,7 +27,7 @@ class GovernorateReportController extends Controller
                                      AS total"),
                             DB::raw("DATE_FORMAT(date, '%m') AS month")
                             )
-                        ->groupBy("month")
+                        ->groupBy(['month', 'date'])
                         ->get();
 
         foreach ($totals as $total) {
