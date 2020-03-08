1d6ec77fedbb536553bf6e02188370c3f1150455
Shady Alfred <shadyalfred98@gmail.com>
2020-03-08 15:34:14 +0200

    Added Https Protocol

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             'localization',
+            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
